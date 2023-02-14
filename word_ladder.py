25ac2cc66efaaac3715535dfd7c47a78a8512de4
pingff <lfong24@students.claremontmckenna.edu>
2023-02-14 00:18:17 -0800

    wordladder

--- word_ladder.py
+++ word_ladder.py
@@ -1,5 +1,8 @@
 #!/bin/python3
 
+from collections import deque
+import copy
+
 
 def word_ladder(start_word, end_word, dictionary_file='words5.dict'):
     '''
@@ -28,7 +31,30 @@ def word_ladder(start_word, end_word, dictionary_file='words5.dict'):
     Whenever it is impossible to generate a word ladder between the two words,
     the function returns `None`.
     '''
-
+    if len(start_word) != len(end_word):
+        return None
+    stack = []
+    dictionary_file = get_text("words5.dict")
+    dictionary = dictionary_file.split("\n")
+    stack.append(start_word)
+    dictionary.remove(start_word)
+    xs = deque([])
+    xs.append(stack)
+    if start_word == end_word:
+        return xs
+    while len(xs) != 0:
+        ref = xs.popleft()
+        copy_dict = copy.copy(dictionary)
+        for word in copy_dict:
+            if _adjacent(word, ref[-1]):
+                if word == end_word:
+                    ref.append(word)
+                    return ref
+                lc = copy.copy(ref)
+                lc.append(word)
+                xs.append(lc)
+                dictionary.remove(word)
+    return None
 
 def verify_word_ladder(ladder):
     '''
@@ -40,7 +66,13 @@ def verify_word_ladder(ladder):
     >>> verify_word_ladder(['stone', 'shone', 'phony'])
     False
     '''
-
+    if len(ladder) == 0:
+        return False
+    for i in range(len(ladder)):
+        if i == len(ladder) - 1:
+            return True
+        if not _adjacent(ladder[i], ladder[i+1]):
+            return False
 
 def _adjacent(word1, word2):
     '''
@@ -52,3 +84,18 @@ def _adjacent(word1, word2):
     >>> _adjacent('stone','money')
     False
     '''
+    list = []
+    if len(word1) != len(word2):
+        return False
+    for i in range(len(word1)):
+        if word1[i] != word2[i]:
+            list += [i]
+    if len(list) != 1:
+        return False
+    else:
+        return True 
+
+def get_text(filename):
+    with open(filename, encoding='latin1') as f:
+        text = f.read()
+    return text
